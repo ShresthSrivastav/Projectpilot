d149fcffff53a7f7410f7074bd22b71d8c8c971e
SHRESTH SRIVASTAV <shresthsrivastav01@gmail.com>
2026-06-25 15:56:10 +0530

    fix: terraform fmt alignment for source_id

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -24,7 +24,7 @@ resource "oci_core_instance" "app" {
 
   source_details {
     source_type = "image"
-    source_id      = data.oci_core_images.os.images[0].id
+    source_id   = data.oci_core_images.os.images[0].id
   }
 
   create_vnic_details {
